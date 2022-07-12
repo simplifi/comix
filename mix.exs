2350f84bbc08816858d334a6d299a2d42369500a
Liru Færs <liru@simpli.fi>
2022-07-12 12:53:28 -0400

    Enable and configure packaging

--- mix.exs
+++ mix.exs
@@ -1,11 +1,16 @@
 defmodule ExSifiComix.MixProject do
   use Mix.Project
 
+  defp github, do: "https://github.com/simplifi/ex_sifi_comix"
+
   def project do
     [
       app: :ex_sifi_comix,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12",
+      package: package(),
+      description: "Common mix.exs code that all projects can benefit from",
+      source_url: github(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +26,18 @@ defmodule ExSifiComix.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:git_cli, "~> 0.3"},
       {:temp, "~> 0.4", only: :test}
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["RTB"],
+      organization: "simplifi",
+      links: %{Github: github()}
     ]
   end
 end
